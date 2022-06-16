014cee9fbb75915acd7de2dbaa2cf549d26b14cf
Dzianis Selivanau <>
2022-06-17 00:34:55 +0300

    Added initial project

---
+++ EveryDay/EveryDay/EveryDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryDayApp.swift
+//  EveryDay
+//
+//  Created by Dzianis Selivanau on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct EveryDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
