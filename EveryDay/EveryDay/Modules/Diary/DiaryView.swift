02277f96149433f42e7239bb2ba066231f0b5310
Denis Selivanov <40215783+dselivanau@users.noreply.github.com>
2022-06-25 00:02:53 +0300

    Splash screen (#1)
    * Added app icon
    
    * Added logo to default launch screen
    
    * Added initial launch screen
    
    * Added initial launch screen markup
    
    * Added custom text view
    
    * Added root tabview
    
    Co-authored-by: Dzianis Selivanau <>

---
+++ EveryDay/EveryDay/Modules/Diary/DiaryView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryView.swift
+//  EveryDay
+//
+//  Created by Dzianis Selivanau on 23.06.22.
+//
+
+import SwiftUI
+
+struct DiaryView: View {
+    var body: some View {
+        Text("Diary view!")
+    }
+}
+
+struct DiaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryView()
+    }
+}
---
+++ EveryDay/EveryDay/Modules/LaunchScreen/LaunchView.swift
@@ -0,0 +1,39 @@
+//
+//  LaunchView.swift
+//  EveryDay
+//
+//  Created by Dzianis Selivanau on 19.06.22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        ZStack(){
+            Image("launch")
+                .resizable()
+                .ignoresSafeArea()
+                .aspectRatio(contentMode: .fill)
+            VStack(spacing: 0) {
+                HStack(){
+                    AdlerySwashText(title: "EveryDay", size: 72, foregroundColor: .white)
+                }
+                .frame(maxWidth: .infinity)
+                HStack(){
+                    Spacer()
+                    AdlerySwashText(title: "твой личный дневник", size: 24, foregroundColor: .white)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, 250)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+        }
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
